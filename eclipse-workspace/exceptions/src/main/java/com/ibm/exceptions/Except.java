0421fb3692e9f067cf0ef5309bbd356877025549
GaneshReddy Jyothi <Ganesh.Reddy.Jyothi@ibm.com>
2025-05-02 17:13:18 +0530

    Exceptions adding

--- eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Except.java
+++ eclipse-workspace/exceptions/src/main/java/com/ibm/exceptions/Except.java
@@ -1,21 +1,33 @@
 package com.ibm.exceptions;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Except {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		try {
-			System.out.println(args[0]);
+			Scanner sc = new Scanner(System.in);
+			int num1 =sc.nextInt();
+			int num2=sc.nextInt();
+			try {
+			int div= num1/num2;
+			//System.exit(0); // finally block not always executed 100%
+			System.out.println(div);
+			}catch(ArithmeticException exception) {
+				System.out.println("can't divide by zero and Nested Catch");
+			}
+			finally {
+				System.out.println("End of outer try");
+			}
 		}
-		catch(ArrayIndexOutOfBoundsException e) {
+		catch(InputMismatchException | NumberFormatException e) {
 			System.out.println("Run the program supplying a number or text");
 		}
 		catch(ArithmeticException ex) {
 			System.out.println("You cannot divide by zero");
-		}
-		catch(NumberFormatException ex) {
-			System.out.println("Please provide valid number");
 		}
 		catch(Exception e) {
 			e.printStackTrace();
